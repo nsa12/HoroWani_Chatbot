c9b99b039a3644e272830c11f4a64d2722f944b1
nsa12 <singh.navjot12@gmail.com>
2016-09-16 13:56:28 +0530

    blah

--- somebot/urls.py
+++ somebot/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', index),
+    url(r'^$', somebot.views.index),
     url(r'^facebook_auth/?$', MyChatBotView.as_view()),
 )
